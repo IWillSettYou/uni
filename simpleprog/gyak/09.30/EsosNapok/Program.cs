605ebdf213a38c9f09d2b1cd55954ca73792a681
iwillsettyou <bmate20050911@gmail.com>
2025-09-30 12:01:52 +0200

    duh

---
+++ simpleprog/gyak/09.30/EsosNapok/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EsosNapok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int MaxN = 100;
+            int n, db = 0;
+            int[] m = new int[MaxN + 1];
+
+            Console.Error.Write("N = ?: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Error.Write($"Esős nap {i} mennyi eső esett?: ");
+                m[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(int i = 1; i <= n; i++)
+            {
+                if (m[i] > 0) db++;
+            }
+
+            Console.WriteLine($"{db}");
+        }
+    }
+}
+
+
+
